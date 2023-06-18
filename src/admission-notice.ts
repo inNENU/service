f33e3ea640c9672931ddba191f4927895e71b2a2
Mr.Hope <mister-hope@outlook.com>
2023-06-18 16:08:07 +0800

    feat: init

---
+++ src/admission-notice.ts
@@ -0,0 +1,57 @@
+import type { RequestHandler } from "express";
+
+interface AdmissionRequestOptions {
+  type: "fetch";
+  level: "本科生" | "研究生";
+}
+
+const getAdmission = async ({ level }: AdmissionRequestOptions) => {
+  if (level === "本科生") {
+    const imageResponse = await fetch(
+      "http://bkzsw.nenu.edu.cn/include/webgetcode.php?width=85&height=28&sitex=15&sitey=6"
+    );
+
+    const cookies = imageResponse.headers.get("Set-Cookie")!;
+
+    const base64Image = `data:image/png;base64,${Buffer.from(
+      await imageResponse.arrayBuffer()
+    ).toString("base64")}`;
+
+    const headers = new Headers({});
+
+    headers.append("Cookie", cookies);
+
+    const infoResponse = await fetch(
+      "http://bkzsw.nenu.edu.cn/col_000018_000169.html",
+      { headers }
+    );
+
+    const infoBody = await infoResponse.text();
+
+    const [, notice] = /<td colspan="2" align="left">截止 (.*?)<\/td>/.exec(
+      infoBody
+    )!;
+
+    return {
+      cookies: cookies,
+      info: ["name", "id", "testId"],
+      verifyCode: base64Image,
+      notice: "部分省份信息正在录入中，点击查看详情",
+      detail: {
+        title: "录取信息",
+        content: notice.replace(/<br>/g, "\n"),
+      },
+    };
+  }
+};
+
+export const admissionNoticeHandler: RequestHandler = (req, res) => {
+  console.log(req.body);
+  const { body } = req;
+
+  console.log(body);
+
+  if (body.type === "fetch") {
+    getAdmission(body).then((data) => res.send(data));
+  }
+};
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { admissionNoticeHandler } from "./admission-notice.js";
+
+const app = express();
+const port = 8080;
+
+app.get("/", (_req, res) => {
+  res.send("这里是 in 东师服务");
+});
+app.post("/admission-notice", admissionNoticeHandler);
+
+app.listen(port, () => {
+  console.log(`Service listening on port ${port}`);
+});
