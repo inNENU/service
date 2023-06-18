1bf5bf88ca7b05f9b83a49e525ee38655ae50da0
Mr.Hope <mister-hope@outlook.com>
2023-06-18 16:15:51 +0800

    chore: tweaks

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import compression from "compression";
 import express from "express";
 import { admissionNoticeHandler } from "./admission-notice.js";
 
 const app = express();
 const port = 8080;
+
+app.use(compression);
 
 app.get("/", (_req, res) => {
   res.send("这里是 in 东师服务");
