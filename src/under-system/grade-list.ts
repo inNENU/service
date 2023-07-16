aa2b713bc48bdd83ca2e162358e9addfa661a57f
Mr.Hope <mister-hope@outlook.com>
2023-07-16 17:00:32 +0800

    chore: remove log

--- src/under-system/grade-list.ts
+++ src/under-system/grade-list.ts
@@ -166,13 +166,12 @@ const getScoreDetail = (content: string): ScoreDetail | null => {
 export const getGrades = (
   cookieHeader: string,
   content: string,
-  isJS = false,
+  isJS = false
 ): Promise<GradeResult[]> =>
   Promise.all(
     Array.from(
-      content.matchAll(isJS ? jsGradeItemRegExp : gradeItemRegExp),
+      content.matchAll(isJS ? jsGradeItemRegExp : gradeItemRegExp)
     ).map(async ([, item]) => {
-      console.log(item, gradeCellRegExp.exec(item)!);
       const [
         ,
         time,
@@ -191,7 +190,7 @@ export const getGrades = (
         reLearn,
         status,
       ] = Array.from(gradeCellRegExp.exec(item)!).map((item) =>
-        item.replace(/&nbsp;/g, " ").trim(),
+        item.replace(/&nbsp;/g, " ").trim()
       );
       const [, gradeLink, gradeNumber] = gradeRegExp.exec(grade) || [];
       const actualDifficulty = Number(difficulty) || 1;
@@ -200,7 +199,7 @@ export const getGrades = (
         gradeNumber && !Number.isNaN(Number(gradeNumber))
           ? Number(gradeNumber)
           : Math.round(
-              (Number(gradePoint) / Number(point) / actualDifficulty) * 10 + 50,
+              (Number(gradePoint) / Number(point) / actualDifficulty) * 10 + 50
             );
 
       let gradeDetail: GradeDetail | null = null;
@@ -262,12 +261,12 @@ export const getGrades = (
         reLearn: reLearn ? getDisplayTime(reLearn) : "",
         status,
       };
-    }),
+    })
   );
 
 export const getGradeLists = async (
   cookieHeader: string,
-  content: string,
+  content: string
 ): Promise<GradeResult[]> => {
   // We force writing these 2 field to ensure we care getting the default table structure
   const tableFields = tableFieldsRegExp.exec(content)![1];
@@ -333,7 +332,7 @@ export const getGradeLists = async (
       const newGrades = await getGrades(cookieHeader, responseText, true);
 
       grades.push(...newGrades);
-    }),
+    })
   );
 
   return grades;
@@ -399,7 +398,6 @@ export const underGradeListHandler: RequestHandler<
 
     return res.json(<UserGradeListSuccessResponse>{
       success: true,
-
       data: gradeList,
     });
   } catch (err) {
