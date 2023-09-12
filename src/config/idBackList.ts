4c1d364c70bba94db5c5d67cb51b8a6bc3adf715
Mr.Hope <mister-hope@outlook.com>
2023-09-12 18:30:24 +0800

    chore: update blacklist

--- src/config/idBackList.ts
+++ src/config/idBackList.ts
@@ -1,6 +1,6 @@
 import type { MyInfo } from "../my/index.js";
 
-export const ID_BLACK_LIST = [2021012638, 2023014072, 2023010767];
+export const ID_BLACK_LIST = [2021012638, 2023014072, 2023010767, 2023013629];
 
 export type IdConditionalBlackList = {
   [I in keyof MyInfo]?: MyInfo[I] | RegExp;
@@ -18,10 +18,5 @@ export const ID_CONDITIONAL_BLACK_LIST: IdConditionalBlackList[] = [
     major: "5rGJ6K+t6KiA5paH5a2m",
     grade: 2023,
     idCard: /^220602/,
-  },
-  {
-    name: "5YiY5rKB6ZuF",
-    grade: 2023,
-    org: "57uP5rWO5LiO566h55CG5a2m6Zmi",
   },
 ];
