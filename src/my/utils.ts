caa7e1a6ccfeab777d95178e4100c8fb09fb8aee
Mr.Hope <mister-hope@outlook.com>
2024-06-09 18:59:49 +0800

    fix: fix my server

--- src/my/utils.ts
+++ src/my/utils.ts
@@ -1,2 +1,2 @@
-export const MY_SERVER = "https://my.webvpn.nenu.edu.cn";
+export const MY_SERVER = "https://my-443.webvpn.nenu.edu.cn";
 export const MY_MAIN_PAGE = `${MY_SERVER}/portal_main/toPortalPage`;
