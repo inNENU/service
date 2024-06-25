32c80bdfb497dd6da089f71f8d7fb95a22e7d67d
Mister-Hope <mister-hope@outlook.com>
2024-06-25 19:50:53 +0800

    feat: support 2024 under plan

--- src/enroll/under-plan.ts
+++ src/enroll/under-plan.ts
@@ -1,161 +1,112 @@
 import type { RequestHandler } from "express";
 
-import { InvalidArgResponse, UnknownResponse } from "../config/index.js";
-import type { CommonFailedResponse, EmptyObject } from "../typings.js";
+import type { ActionFailType } from "../config/index.js";
+import {
+  InvalidArgResponse,
+  MissingArgResponse,
+  UnknownResponse,
+} from "../config/index.js";
+import type {
+  CommonFailedResponse,
+  CommonSuccessResponse,
+  EmptyObject,
+} from "../typings.js";
 
-export interface UnderEnrollInfoOptions {
+export interface UnderEnrollPlanInfoOptions {
   type: "info";
 }
 
-export interface UnderEnrollInfo {
-  years: string[];
-  provinces: string[];
-}
-
-const UNDER_ENROLL_PAGE_URL =
-  "https://bkzsw.nenu.edu.cn/col_000018_000171.html";
-
-const YEAR_REG_EXP =
-  /<select class="custom-select year" id="inputGroupSelect01">\s*?<option selected>请选择<\/option>\s*((?:<option value=".*">.*<\/option>\s*?)+)<\/select>/;
-const PROVINCE_REG_EXP =
-  /<select class="custom-select province" id="inputGroupSelect01"[^>]*>\s*?<option selected>请选择<\/option>\s*((?:<option value=".*">.*<\/option>\s*?)+)<\/select>/;
-
-const OPTION_REG_EXP = /<option value="(.*?)">.*?<\/option>/g;
-
-const getUnderEnrollInfo = async (): Promise<UnderEnrollInfo> => {
-  const infoResponse = await fetch(UNDER_ENROLL_PAGE_URL);
-  const content = await infoResponse.text();
-
-  const yearOptions = YEAR_REG_EXP.exec(content)?.[1];
-  const provinceOptions = PROVINCE_REG_EXP.exec(content)?.[1];
-
-  if (!yearOptions || !provinceOptions) {
-    throw new Error("获取省份和年份信息失败");
-  }
-
-  const years = Array.from(yearOptions.matchAll(OPTION_REG_EXP)).map(
-    ([, value]) => value,
-  );
-  const provinces = Array.from(provinceOptions.matchAll(OPTION_REG_EXP)).map(
-    ([, value]) => value,
-  );
-
-  return {
-    years,
-    provinces,
-  };
-};
-
-export interface UnderEnrollPlanTypeOptions {
-  type: "planType";
-  province: string;
-  year: string;
-}
-
-const UNDER_ENROLL_PLAN_URL = "https://bkzsw.nenu.edu.cn/getPlanOpt";
-
-const getUnderEnrollPlanType = async ({
-  year,
-  province,
-}: UnderEnrollPlanTypeOptions): Promise<string[]> => {
-  const searchResponse = await fetch(UNDER_ENROLL_PLAN_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ typename: "plan", year, province }),
-  });
-
-  const plans = (await searchResponse.json()) as string[];
-
-  return plans;
-};
-
-export interface UnderEnrollMajorTypeOptions {
-  type: "majorType";
-  province: string;
-  year: string;
-  plan: string;
-}
-
-const UNDER_ENROLL_MAJOR_TYPE_URL = "https://bkzsw.nenu.edu.cn/getMajorTypeOpt";
-
-const getUnderEnrollMajorType = async ({
-  year,
-  province,
-  plan,
-}: UnderEnrollMajorTypeOptions): Promise<string[]> => {
-  const searchResponse = await fetch(UNDER_ENROLL_MAJOR_TYPE_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ typename: "plan", year, province, plan }),
-  });
-
-  const plans = (await searchResponse.json()) as string[];
-
-  return plans;
-};
-
-export interface UnderEnrollMajorClassOptions {
-  type: "majorClass";
-  province: string;
-  year: string;
-  plan: string;
-  majorType: string;
-}
-
-const UNDER_ENROLL_MAJOR_CLASS_URL =
-  "https://bkzsw.nenu.edu.cn/getMajorClassOpt";
-
-const getUnderEnrollMajorClass = async ({
-  year,
-  province,
-  plan,
-  majorType,
-}: UnderEnrollMajorClassOptions): Promise<string[]> => {
-  const searchResponse = await fetch(UNDER_ENROLL_MAJOR_CLASS_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      typename: "plan",
-      year,
-      province,
-      plan,
-      major_type: majorType,
-    }),
-  });
-
-  const plans = (await searchResponse.json()) as string[];
-
-  return plans;
-};
-
 export interface UnderEnrollPlanQueryOptions {
   type: "query";
-  majorClass: string;
-  majorType: string;
-  plan: string;
+  /** 省份 */
   province: string;
+  /** 年份 */
+  year: string;
+  /**类型 */
+  classType: string;
+  /** 专业类型 */
+  majorType: string;
+}
+
+export type UnderEnrollPlanOptions =
+  | UnderEnrollPlanInfoOptions
+  | UnderEnrollPlanQueryOptions;
+
+const INFO_URL = "https://gkcx.nenu.edu.cn/api/user/param";
+
+interface RawUnderEnrollPlanOptionConfig {
+  major_type: string;
+  type: string;
   year: string;
 }
 
-export interface RawUnderEnrollPlanConfig {
+type RawUnderEnrollPlanOptionInfo = Record<
+  /* province */ string,
+  RawUnderEnrollPlanOptionConfig[]
+>;
+
+type UnderEnrollOptionInfo = Record<
+  /* province */ string,
+  Record<
+    /* year */ string,
+    Record</* type */ string, /* major type */ string[]>
+  >
+>;
+
+export type UnderEnrollPlanInfoSuccessResponse =
+  CommonSuccessResponse<UnderEnrollOptionInfo>;
+
+export type UnderEnrollPlanInfoResponse =
+  | UnderEnrollPlanInfoSuccessResponse
+  | CommonFailedResponse;
+
+export const getUnderEnrollInfo =
+  async (): Promise<UnderEnrollPlanInfoResponse> => {
+    // NOTE: year=2024 does not take any effect
+    const infoResponse = await fetch(`${INFO_URL}?which=plan`);
+
+    const info = (await infoResponse.json()) as RawUnderEnrollPlanOptionInfo;
+
+    return {
+      success: true,
+      data: Object.fromEntries(
+        Object.entries(info).map(([province, configs]) => {
+          const result: Record<string, Record<string, string[]>> = {};
+
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          configs.forEach(({ major_type, type, year }) => {
+            ((result[year] ??= {})[type] ??= []).push(major_type);
+          });
+
+          return [province, result];
+        }),
+      ),
+    };
+  };
+
+interface RawUnderEnrollPlanConfig {
+  edu_cost: string;
+  edu_len: string;
   major: string;
   major_attr: string;
-  admission_num: string;
-  admission_len: string;
-  admission_cost: string;
+  major_type: string;
+  number: string;
+  province: string;
+  type: string;
+  memo: { String: ""; Valid: false };
+}
+
+interface RawUnderEnrollPlanResult {
+  message: "Success";
+  year: string;
+  plans: RawUnderEnrollPlanConfig[];
 }
 
 export interface UnderEnrollPlanConfig {
   /** 专业 */
   major: string;
-  /** 专业类别 */
-  majorType: string;
+  /** 专业属性 */
+  majorAttr: string;
   /** 招生计划 */
   count: string;
   /** 学制 */
@@ -164,65 +115,65 @@ export interface UnderEnrollPlanConfig {
   fee: string;
 }
 
-const QUERY_URL = "https://bkzsw.nenu.edu.cn/queryPlan";
+type UnderEnrollPlanQuerySuccessResponse = CommonSuccessResponse<
+  UnderEnrollPlanConfig[]
+>;
 
-const getUnderEnrollPlans = async (
-  options: UnderEnrollPlanQueryOptions,
-): Promise<UnderEnrollPlanConfig[]> => {
-  const searchResponse = await fetch(QUERY_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
+type UnderEnrollPlanQueryResponse =
+  | UnderEnrollPlanQuerySuccessResponse
+  | CommonFailedResponse<ActionFailType.MissingArg | ActionFailType.Unknown>;
+
+export const queryUnderEnrollPlan = async ({
+  province,
+  year,
+  classType,
+  majorType,
+}: UnderEnrollPlanQueryOptions): Promise<UnderEnrollPlanQueryResponse> => {
+  if (!province) return MissingArgResponse("province");
+
+  if (!year) return MissingArgResponse("year");
+
+  if (!classType) return MissingArgResponse("classType");
+
+  if (!majorType) return MissingArgResponse("majorType");
+
+  const queryResponse = await fetch(
+    `https://gkcx.nenu.edu.cn/api/user/queryPlan`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({
+        province,
+        year,
+        major_type: classType,
+        type: majorType,
+      }),
     },
-    body: JSON.stringify(options),
-  });
-
-  const rawPlans = (await searchResponse.json()) as RawUnderEnrollPlanConfig[];
-
-  return rawPlans.map(
-    ({
-      major,
-      major_attr: majorType,
-      admission_num: count,
-      admission_len: years,
-      admission_cost: fee,
-    }) => ({
-      major,
-      majorType,
-      count,
-      years,
-      fee,
-    }),
   );
+
+  const result = (await queryResponse.json()) as RawUnderEnrollPlanResult;
+
+  return {
+    success: true,
+    data: result.plans.map(
+      ({
+        major,
+        major_attr: majorAttr,
+        number,
+        edu_len: years,
+        edu_cost: fee,
+      }) => ({
+        major,
+        majorAttr,
+        count: number,
+        years,
+        fee,
+      }),
+    ),
+  };
 };
-
-export interface UnderEnrollInfoSuccessResponse {
-  success: true;
-  data: UnderEnrollInfo;
-}
-
-export interface UnderEnrollDetailsSuccessResponse {
-  success: true;
-  data: string[];
-}
-
-export interface UnderEnrollPlanSuccessResponse {
-  success: true;
-  data: UnderEnrollPlanConfig[];
-}
-
-export type UnderEnrollPlanOptions =
-  | UnderEnrollInfoOptions
-  | UnderEnrollPlanTypeOptions
-  | UnderEnrollMajorClassOptions
-  | UnderEnrollMajorTypeOptions
-  | UnderEnrollPlanQueryOptions;
-
-export type UnderEnrollPlanResponse =
-  | UnderEnrollInfoSuccessResponse
-  | UnderEnrollDetailsSuccessResponse
-  | UnderEnrollPlanSuccessResponse
-  | CommonFailedResponse;
 
 export const underEnrollPlanHandler: RequestHandler<
   EmptyObject,
@@ -230,51 +181,12 @@ export const underEnrollPlanHandler: RequestHandler<
   UnderEnrollPlanOptions
 > = async (req, res) => {
   try {
-    const { type } = req.body;
-
-    if (type === "info") {
-      const info = await getUnderEnrollInfo();
-
-      return res.json({
-        success: true,
-        data: info,
-      } as UnderEnrollInfoSuccessResponse);
+    if (req.body.type === "info") {
+      return res.json(await getUnderEnrollInfo());
     }
 
-    if (type === "planType") {
-      const plans = await getUnderEnrollPlanType(req.body);
-
-      return res.json({
-        success: true,
-        data: plans,
-      } as UnderEnrollDetailsSuccessResponse);
-    }
-
-    if (type === "majorType") {
-      const majorTypes = await getUnderEnrollMajorType(req.body);
-
-      return res.json({
-        success: true,
-        data: majorTypes,
-      } as UnderEnrollDetailsSuccessResponse);
-    }
-
-    if (type === "majorClass") {
-      const majorClasses = await getUnderEnrollMajorClass(req.body);
-
-      return res.json({
-        success: true,
-        data: majorClasses,
-      } as UnderEnrollDetailsSuccessResponse);
-    }
-
-    if (type === "query") {
-      const data = await getUnderEnrollPlans(req.body);
-
-      return res.json({
-        success: true,
-        data,
-      } as UnderEnrollPlanSuccessResponse);
+    if (req.body.type === "query") {
+      return res.json(await queryUnderEnrollPlan(req.body));
     }
 
     return res.json(InvalidArgResponse("type"));
