87c3777d5062197d2bbbb385ef8246c94743381a
Mr.Hope <mister-hope@outlook.com>
2024-08-25 02:32:08 +0800

    fix: catch all errors

--- src/utils/handler.ts
+++ src/utils/handler.ts
@@ -1,14 +1,23 @@
 import type { RequestHandler } from "express";
 import type { ParamsDictionary, Query } from "express-serve-static-core";
 
-import type { EmptyObject } from "../typings";
+import type { EmptyObject } from "../typings.js";
 
-export const middleware = <
-  ResBody = unknown,
-  ReqBody = EmptyObject,
-  ReqQuery = Query,
-  Params = ParamsDictionary,
-  Locals extends Record<string, unknown> = EmptyObject,
->(
-  handler: RequestHandler<Params, ResBody, ReqBody, ReqQuery, Locals>,
-): RequestHandler<Params, ResBody, ReqBody, ReqQuery, Locals> => handler;
+export const middleware =
+  <
+    ResBody = unknown,
+    ReqBody = EmptyObject,
+    ReqQuery = Query,
+    Params = ParamsDictionary,
+    Locals extends Record<string, unknown> = EmptyObject,
+  >(
+    handler: RequestHandler<Params, ResBody, ReqBody, ReqQuery, Locals>,
+  ): RequestHandler<Params, ResBody, ReqBody, ReqQuery, Locals> =>
+  async (req, res, next) => {
+    try {
+      // eslint-disable-next-line @typescript-eslint/await-thenable
+      return await handler(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
