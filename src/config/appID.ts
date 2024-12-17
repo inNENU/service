8291568371adabccb223768a3f5b2b7c62ff89a9
Mr.Hope <mister-hope@outlook.com>
2024-12-17 16:51:01 +0800

    chore: bump deps

--- src/config/appID.ts
+++ src/config/appID.ts
@@ -3,10 +3,12 @@ import "./loadEnv.js";
 export const rawID2appID: Record<string, string> = {
   gh_ef5423e51c35: "wx33acb831ee1831a5",
   gh_adfb1b973ed6: "wx2550e3fd373b79a8",
+  gh_3f183bc1aa09: "wx2d632391509810f8",
 };
 
 export const appIDInfo = {
   1109559721: process.env.QQ_API_KEY,
   wx33acb831ee1831a5: process.env.WECHAT_API_KEY,
   wx2550e3fd373b79a8: process.env.WE_API_KEY,
+  wx2d632391509810f8: process.env.ZAI_API_KEY,
 } as const;
