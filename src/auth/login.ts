05a5936c1f3d94e32f0ad25bd23e28240f73c011
Mr.Hope <mister-hope@outlook.com>
2025-06-23 21:07:36 +0800

    feat: update login url

--- src/auth/login.ts
+++ src/auth/login.ts
@@ -106,13 +106,161 @@ export const authLogin = async ({
     };
   }
 
-  if (loginPageResponse.status === 200) {
-    const content = await loginPageResponse.text();
+  if (loginPageResponse.status !== 200) {
+    console.error(
+      "Unknown login page status: ",
+      loginPageResponse.status,
+      await loginPageResponse.text(),
+    );
 
+    return UnknownResponse("未知错误");
+  }
+
+  const content = await loginPageResponse.text();
+
+  if (
+    content.includes("不允许使用认证服务来认证您访问的目标应用。") ||
+    content.includes("不允许访问")
+  ) {
+    console.error("Forbidden service", service, "for id", id);
+
+    return {
+      success: false,
+      type: ActionFailType.Forbidden,
+      msg: "用户账号没有此服务权限。",
+    };
+  }
+
+  const salt = SALT_REGEXP.exec(content)![1];
+  const execution = /name="execution" value="(.*?)"/.exec(content)![1];
+
+  cookieStore.set({
+    name: "org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE",
+    value: "zh_CN",
+    domain,
+  });
+
+  const checkCaptchaLink = `${server}/authserver/checkNeedCaptcha.htl?username=${id}&_=${Date.now()}`;
+
+  const captchaCheckResponse = await fetch(checkCaptchaLink, {
+    headers: {
+      Cookie: cookieStore.getHeader(checkCaptchaLink),
+      Referer: AUTH_LOGIN_URL,
+      "User-Agent": "inNENU service",
+    },
+  });
+
+  cookieStore.applyResponse(captchaCheckResponse, server);
+
+  const { isNeed: needCaptcha } =
+    await (captchaCheckResponse.json() as Promise<{
+      isNeed: boolean;
+    }>);
+
+  if (needCaptcha)
+    return {
+      success: false,
+      type: ActionFailType.NeedCaptcha,
+      msg: "需要验证码，请重新登录",
+    };
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Cookie: cookieStore.getHeader(url),
+      Referer: url,
+      "User-Agent": "inNENU service",
+    },
+    body: new URLSearchParams({
+      username: id.toString(),
+      password: authEncrypt(password, salt),
+      lt: "",
+      cllt: "usernameLogin",
+      dllt: "generalLogin",
+      execution,
+      _eventId: "submit",
+      rememberMe: "true",
+    }),
+    redirect: "manual",
+  });
+
+  const resultContent = await response.text();
+  const location = response.headers.get("Location");
+
+  cookieStore.applyResponse(response, server);
+
+  if (response.status === 302) {
+    if (location?.startsWith(`${server}/authserver/login`))
+      return WrongPasswordResponse;
+
+    return {
+      success: true,
+      cookieStore,
+      location: location!,
+    };
+  }
+
+  if (response.status === 401) {
     if (
-      content.includes("不允许使用认证服务来认证您访问的目标应用。") ||
-      content.includes("不允许访问")
-    ) {
+      resultContent.includes("该账号非常用账号或用户名密码有误") ||
+      resultContent.includes("您提供的用户名或者密码有误")
+    )
+      return WrongPasswordResponse;
+
+    if (resultContent.includes("图形动态码错误"))
+      return {
+        success: false,
+        type: ActionFailType.WrongCaptcha,
+        msg: "图形动态码错误，请重试",
+      };
+
+    if (resultContent.includes("该帐号已经被禁用"))
+      return {
+        success: false,
+        type: ActionFailType.Forbidden,
+        msg: "该帐号已经被禁用",
+      };
+
+    const lockedResult = /<span>账号已冻结，预计解冻时间：(.*?)<\/span>/.exec(
+      resultContent,
+    );
+
+    if (lockedResult)
+      return {
+        success: false,
+        type: ActionFailType.AccountLocked,
+        msg: `账号已冻结，预计解冻时间：${lockedResult[1]}`,
+      };
+
+    console.error("Unknown login response: ", response.status, resultContent);
+
+    return UnknownResponse("未知错误");
+  }
+
+  if (response.status === 200) {
+    if (resultContent.includes("会话已失效，请刷新页面再登录"))
+      return {
+        success: false,
+        type: ActionFailType.Expired,
+        msg: "会话已过期，请重新登录",
+      };
+
+    if (resultContent.includes("当前账户已在其他PC端登录会话。"))
+      return {
+        success: false,
+        type: ActionFailType.EnabledSSO,
+        msg: "您已开启单点登录，请访问学校统一身份认证官网，在个人设置中关闭单点登录后重试。",
+      };
+
+    if (resultContent.includes("<span>请输入验证码</span>"))
+      return {
+        success: false,
+        type: ActionFailType.NeedCaptcha,
+        msg: "需要验证码，请重新登录。",
+      };
+
+    if (resultContent.includes("不允许使用认证服务来认证您访问的目标应用。")) {
       console.error("Forbidden service", service, "for id", id);
 
       return {
@@ -121,155 +269,9 @@ export const authLogin = async ({
         msg: "用户账号没有此服务权限。",
       };
     }
-
-    const salt = SALT_REGEXP.exec(content)![1];
-    const execution = /name="execution" value="(.*?)"/.exec(content)![1];
-
-    cookieStore.set({
-      name: "org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE",
-      value: "zh_CN",
-      domain,
-    });
-
-    const checkCaptchaLink = `${server}/authserver/checkNeedCaptcha.htl?username=${id}&_=${Date.now()}`;
-
-    const captchaCheckResponse = await fetch(checkCaptchaLink, {
-      headers: {
-        Cookie: cookieStore.getHeader(checkCaptchaLink),
-        Referer: AUTH_LOGIN_URL,
-        "User-Agent": "inNENU service",
-      },
-    });
-
-    cookieStore.applyResponse(captchaCheckResponse, server);
-
-    const { isNeed: needCaptcha } =
-      await (captchaCheckResponse.json() as Promise<{
-        isNeed: boolean;
-      }>);
-
-    if (needCaptcha)
-      return {
-        success: false,
-        type: ActionFailType.NeedCaptcha,
-        msg: "需要验证码，请重新登录",
-      };
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Cookie: cookieStore.getHeader(url),
-        Referer: url,
-        "User-Agent": "inNENU service",
-      },
-      body: new URLSearchParams({
-        username: id.toString(),
-        password: authEncrypt(password, salt),
-        lt: "",
-        cllt: "usernameLogin",
-        dllt: "generalLogin",
-        execution,
-        _eventId: "submit",
-        rememberMe: "true",
-      }),
-      redirect: "manual",
-    });
-
-    const resultContent = await response.text();
-    const location = response.headers.get("Location");
-
-    cookieStore.applyResponse(response, server);
-
-    if (response.status === 401) {
-      if (
-        resultContent.includes("该账号非常用账号或用户名密码有误") ||
-        resultContent.includes("您提供的用户名或者密码有误")
-      )
-        return WrongPasswordResponse;
-
-      if (resultContent.includes("图形动态码错误"))
-        return {
-          success: false,
-          type: ActionFailType.WrongCaptcha,
-          msg: "图形动态码错误，请重试",
-        };
-
-      if (resultContent.includes("该帐号已经被禁用"))
-        return {
-          success: false,
-          type: ActionFailType.Forbidden,
-          msg: "该帐号已经被禁用",
-        };
-
-      const lockedResult = /<span>账号已冻结，预计解冻时间：(.*?)<\/span>/.exec(
-        resultContent,
-      );
-
-      if (lockedResult)
-        return {
-          success: false,
-          type: ActionFailType.AccountLocked,
-          msg: `账号已冻结，预计解冻时间：${lockedResult[1]}`,
-        };
-
-      console.error("Unknown login response: ", response.status, resultContent);
-
-      return UnknownResponse("未知错误");
-    }
-
-    if (response.status === 200) {
-      if (resultContent.includes("会话已失效，请刷新页面再登录"))
-        return {
-          success: false,
-          type: ActionFailType.Expired,
-          msg: "会话已过期，请重新登录",
-        };
-
-      if (resultContent.includes("当前账户已在其他PC端登录会话。"))
-        return {
-          success: false,
-          type: ActionFailType.EnabledSSO,
-          msg: "您已开启单点登录，请访问学校统一身份认证官网，在个人设置中关闭单点登录后重试。",
-        };
-
-      if (resultContent.includes("<span>请输入验证码</span>"))
-        return {
-          success: false,
-          type: ActionFailType.NeedCaptcha,
-          msg: "需要验证码，请重新登录。",
-        };
-
-      if (
-        resultContent.includes("不允许使用认证服务来认证您访问的目标应用。")
-      ) {
-        console.error("Forbidden service", service, "for id", id);
-
-        return {
-          success: false,
-          type: ActionFailType.Forbidden,
-          msg: "用户账号没有此服务权限。",
-        };
-      }
-
-      console.error("Unknown login response: ", response.status, resultContent);
-
-      return UnknownResponse("未知错误");
-    }
-
-    if (response.status === 302) {
-      if (location?.startsWith(`${server}/authserver/login`))
-        return WrongPasswordResponse;
-
-      return {
-        success: true,
-        cookieStore,
-        location: location!,
-      };
-    }
   }
 
-  console.error("Unknown login page status: ", loginPageResponse.status);
+  console.error("Unknown login response: ", response.status, resultContent);
 
   return UnknownResponse("未知错误");
 };
