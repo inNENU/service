7dbdeda874368063fdcbc8af2a382747cffdfb54
Mr.Hope <mister-hope@outlook.com>
2025-07-31 21:42:42 +0800

    chore: tweaks

--- src/test/connect.ts
+++ src/test/connect.ts
@@ -1,6 +1,6 @@
 import type { PoolConnection } from "mysql2/promise";
 
-import { getConnection, request } from "../utils/index.js";
+import { getConnection, releaseConnection, request } from "../utils/index.js";
 
 export const testConnectHandler = request(async (_req, res) => {
   let connection: PoolConnection | null = null;
@@ -8,12 +8,12 @@ export const testConnectHandler = request(async (_req, res) => {
   try {
     connection = await getConnection();
 
-    connection?.release();
-
-    res.send("Connected to MySQL");
+    return res.send("Connected to MySQL");
   } catch (error) {
     console.error("Error connecting to MySQL:", error);
 
     return res.status(500).send("Error connecting to MySQL");
+  } finally {
+    releaseConnection(connection);
   }
 });
